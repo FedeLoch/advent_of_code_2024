98aa4874a8f9729b884a54ac44f5a086dd5c1fc3
Federico Lochbaum <federico.lochbaum@gmail.com>
2024-12-15 16:21:12 +0100

    Day 2: Red-Nosed Reports

--- challenges/Day 2: Red-Nosed Reports/solve.py
+++ challenges/Day 2: Red-Nosed Reports/solve.py
@@ -7,9 +7,18 @@ def all_is_in(list, dir):
 
 is_safe = lambda list: all_is_in(list, range(-3, 1)) or all_is_in(list, range(0, 4))
 
-part_1 = 0
+def is_safe_with_tolerance(list):
+    for i in range(len(list)):
+        l2 = list.copy(); l2.pop(i)
+        if is_safe(l2): return True
+    
+    return False
+
+part_1, part_2 = 0, 0
 with open(input_path) as f:
-    for line in f: part_1 += 1 if is_safe(list(map(int, line[:-1].split(' ')))) else 0
+    for line in f:
+        lvls = list(map(int, line[:-1].split(' ')))
+        part_1 += 1 if is_safe(lvls) else 0; part_2 +=1 if is_safe_with_tolerance(lvls) else 0
 
 print('Part 1: ', part_1)
 print('Part 2: ', part_2)
