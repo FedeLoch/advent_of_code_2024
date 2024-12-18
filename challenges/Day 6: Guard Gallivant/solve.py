e5bdd4cb9bf88f851f6c5da9559e7c2a5692dbe8
Federico Lochbaum <federico.lochbaum@gmail.com>
2024-12-19 00:14:26 +0100

    part 1

--- challenges/Day 6: Guard Gallivant/solve.py
+++ challenges/Day 6: Guard Gallivant/solve.py
@@ -1,8 +1,19 @@
-input_path = 'challenges/Day 6: Guard Gallivant/input0'
+input_path = 'challenges/Day 6: Guard Gallivant/input'
 
-from utils import dict_from_file, DIR
+from utils import dict_from_file, is_valid, TURN, DIR
 guard, _dict = dict_from_file(input_path)
 
-print('guard: ', guard)
-for row in _dict:
-    print(row)
+forward = lambda guard: (guard[0][0] + DIR[guard[1]][0], guard[0][1] + DIR[guard[1]][1])
+
+def next_pos(guard, graph):
+    next = forward(guard)
+    return (guard[0], TURN[guard[1]]) if graph[next[0]][next[1]] == '#' else (next, guard[1])
+
+current, visited, marked = guard, {}, {}
+while is_valid(forward(current), _dict) and (str(current) not in visited):
+    visited[str(current)] = True
+    marked[str(current[0])] = True
+    current = next_pos(current, _dict)
+    if not is_valid(current[0], _dict): break
+
+print('Part 1:', len(marked) + 1)
--- challenges/Day 6: Guard Gallivant/utils.py
+++ challenges/Day 6: Guard Gallivant/utils.py
@@ -1,15 +1,5 @@
-coords = [
-    (1, 0),   #'UP'
-    (-1, 0),  #'DOWN'
-    (0, -1),  #'LEFT'
-    (0, 1),   #'RIGHT' 
-    (-1, -1), # UP LEFT
-    (-1, 1),  # UP RIGHT
-    (1, -1),  # DOWN LEFT
-    (1, 1),   # DOWN RIGHT
-]
-
-DIR = { 'UP': (1, 0),  'DOWN': (-1, 0), 'LEFT': (0, -1), 'RIGHT': (0, 1) }
+TURN = { 'UP': 'RIGHT', 'RIGHT': 'DOWN', 'DOWN': 'LEFT', 'LEFT': 'UP' } # type: ignore
+DIR = { 'UP': (-1, 0),  'DOWN': (1, 0), 'LEFT': (0, -1), 'RIGHT': (0, 1) }
 
 def dict_from_file(input_path):
   row, guard_pos, array = 0, None, []
@@ -20,5 +10,6 @@ def dict_from_file(input_path):
       array.append([num for num in line[:-1]])
       row +=1
 
-  return (guard_pos, DIR['UP']), array
+  return (guard_pos, 'UP'), array
 
+is_valid = lambda pos, _dict: False if pos[0] < 0 or  pos[0]  >= len(_dict) or  pos[1]  < 0 or  pos[1]  >= len(_dict[0]) else True
