01a03ca2b0d028c5683b21911fd68bdb6bde9f97
Federico Lochbaum <federico.lochbaum@gmail.com>
2025-10-05 09:28:16 +0200

    testing some optimizations

--- challenges/Day 13: Claw Contraption/a_star.py
+++ challenges/Day 13: Claw Contraption/a_star.py
@@ -1,4 +1,6 @@
 from queue import PriorityQueue
+from math import gcd
+from functools import reduce
 
 def a_star(graph, initialNode, h):
   pqueue = PriorityQueue()
@@ -25,20 +27,41 @@ def a_star(graph, initialNode, h):
 
 class Prize:
   def __init__(self, a, b, goal):
-    self.a = a
-    self.b = b
-    self.goal = goal
+    gx = reduce(gcd, [a[0], b[0], goal[0]])
+    gy = reduce(gcd, [a[1], b[1], goal[1]])
+    self.a = a[0] // gx, a[1] // gy
+    self.b = b[0] // gx, b[1] // gy
+    self.goal = goal[0] // gx, goal[1] // gy
 
   def h(self):
     return lambda node: abs(self.goal[0] - node[0][0]) + abs(self.goal[1] - node[0][1])
 
+  def reachable(self, node):
+    x, y = node[0]
+    ax, ay = self.a
+    bx, by = self.b
+    gx, gy = self.goal
+    
+    dx = gx - x
+    dy = gy - y
+    det = ax * by - ay * bx
+    if det == 0: return False
+    m = (dx * by - dy * bx) / det
+    n = (ax * dy - ay * dx) / det
+
+    return m.is_integer() and n.is_integer() and m >= 0 and n >= 0
+
   def __getitem__(self, state):
     node, _ = state
+
+    if not self.reachable(node): return []
+    if (node[0][0] > self.goal[0]): return []
+    if (node[0][1] > self.goal[1]): return []
+    
     res = []
     new_a = ((node[0][0] + self.a[0], node[0][1] + self.a[1]), (node[1][0] + 1, node[1][1]))
     new_b = ((node[0][0] + self.b[0], node[0][1] + self.b[1]), (node[1][0], node[1][1] + 1))
-    if (node[0][0] > self.goal[0]): return []
-    if (node[0][1] > self.goal[1]): return []
+
     if (self.h()(new_a) >= 0): res.append(new_a)
     if (self.h()(new_b) >= 0): res.append(new_b)
 
--- challenges/Day 13: Claw Contraption/solve.py
+++ challenges/Day 13: Claw Contraption/solve.py
@@ -2,8 +2,6 @@ from functools import reduce
 input_path = 'challenges/Day 13: Claw Contraption/input'
 from a_star import a_star, Prize
 
-# 3 tokens to push A
-# 1 token to push B
 prizes = []
 with open(input_path) as f:
     a, b, goal = None, None, None
@@ -21,23 +19,7 @@ with open(input_path) as f:
 
 initial_state = ((0, 0), (0, 0))
 
-cost = lambda state: state[1][0] * 3 + state[1][1]
-p = reduce(lambda total, prize: total + cost(a_star(prize, initial_state, prize.h())), prizes, 0)
+cost = lambda node: node[1][0] * 3 + node[1][1]
+part1 = reduce(lambda total, prize: total + cost(a_star(prize, initial_state, prize.h())), prizes, 0)
 
-print('Part 1:', p)
-
-# Button A: X+94, Y+34 -> (94, 34)
-# Button B: X+22, Y+67 -> (22, 67)
-# Prize: X=8400, Y=5400 -> (8400, 5400)
-
-# Button A: X+26, Y+66
-# Button B: X+67, Y+21
-# Prize: X=12748, Y=12176
-
-# Button A: X+17, Y+86
-# Button B: X+84, Y+37
-# Prize: X=7870, Y=6450
-
-# Button A: X+69, Y+23
-# Button B: X+27, Y+71
-# Prize: X=18641, Y=10279
+print('Part 1:', part1)
